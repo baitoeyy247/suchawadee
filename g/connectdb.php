02cb20b04ab5d2ff7dd7ee2100f60c21bfac58c0
baitoeyy247 <suchawdi718@gmail.com>
2026-02-12 15:15:41 +0700

    Update connectdb.php

--- g/connectdb.php
+++ g/connectdb.php
@@ -1,18 +1,8 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>สุชาวดี สุระสิงห์ (ใบเตย)</title>
-</head>
-
-<body>
-<h1>สุชาวดี สุระสิงห์ (ใบเตย)</h1>
-
 
 <?php
 $host = "localhost";
     $user = "root";
-    $pwd = "";
+    $pwd = "Natcha12345";
     $db = "4061db";
     $conn = mysqli_connect($host,$user,$pwd,$db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
     mysqli_query($conn,"SET NAMES utf8");
