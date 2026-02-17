e03d69d79dfbf7b9ff237fa4e3195fe7adc9ded1
baitoeyy247 <suchawdi718@gmail.com>
2026-02-17 09:48:13 +0700

    Add files via upload

---
+++ j/a.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>สุชาวดี สุระสิงห์(ใบเตย)</title>
+</head>
+
+<body>
+
+<h1>ข้อมูลภาค -- สุชาวดี สุระสิงห์ (ใบเตย)</h1>
+
+<form method="post" action="">
+  ชื่อภาค <input type="text" name = "rname" autofocus required>
+  <button type="submit" name="Submit">บันทึก</button>
+</form>
+<br>
+<br>
+
+<?php
+if(isset($_POST['Submit'])) {
+	include_once("connectdb.php");
+	$rname = $_POST['rname'];
+	$sql2 = "INSERT INTO `regions` VALUES (NULL, '{$ranme}')";
+	mysqli_query($conn, $sql2) or die ("insert ไม่ได้");
+}
+
+?>
+
+<table border = "1">
+  <tr>
+     <th>รหัสภาค</th>
+     <th>ชื่อภาค</th>
+   </tr>
+   
+<?php
+    include_once("connectdb.php");
+	$sql="SELECT*FROM `regions` ORDER BY 'r_name' ASC";
+	$rs = mysqli_query($conn , $sql);
+	
+	while($data = mysqli_fetch_array($rs)){
+	
+?> 
+   <tr>
+     <td><?php echo $data['r_id'];?></td>
+     <td><?php echo $data['r_name'];?></td>
+   </tr>
+<?php } ?>
+</table>
+
+</body>
+</html>
