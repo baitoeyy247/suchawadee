7275571f69ddca1828397bff56b7a711d7a32c56
baitoeyy247 <suchawdi718@gmail.com>
2026-02-17 12:50:34 +0700

    Add files via upload

---
+++ j/delete_regions.php
@@ -0,0 +1,11 @@
+<?php
+    include_once("connectdb.php");
+    $id = $_GET['id'];
+    $sql = "DELETE FROM regions WHERE r_id='{$id}'"; 
+    
+    mysqli_query($conn , $sql) or die ("ลบไม่ได้");
+    
+    echo "<script>";
+    echo "window.location='a.php';";
+    echo "</script>";
+?>
