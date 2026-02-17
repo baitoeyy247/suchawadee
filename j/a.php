668b7d54c78fa05b83d1db097262bf6df851c09b
baitoeyy247 <suchawdi718@gmail.com>
2026-02-17 12:51:08 +0700

    Add files via upload

--- j/a.php
+++ j/a.php
@@ -6,45 +6,51 @@
 </head>
 
 <body>
-
-<h1>ข้อมูลภาค -- สุชาวดี สุระสิงห์ (ใบเตย)</h1>
-
-<form method="post" action="">
-  ชื่อภาค <input type="text" name = "rname" autofocus required>
-  <button type="submit" name="Submit">บันทึก</button>
-</form>
+    <h1>งาน i</h1>
+    <h1>สุชาวดี สุระสิงห์(ใบเตย)</h1>
+<form method = "post" action="">
+    ชื่อภาค <input type= "text" name="rname" autofocus required>
+    <button type ="Submit" name = "Submit"> บันทึก </button>
+</FORM>
 <br>
 <br>
 
 <?php
-if(isset($_POST['Submit'])) {
-	include_once("connectdb.php");
-	$rname = $_POST['rname'];
-	$sql2 = "INSERT INTO `regions` VALUES (NULL, '{$ranme}')";
-	mysqli_query($conn, $sql2) or die ("insert ไม่ได้");
-}
+    if(isset($_POST['Submit'])){
+        include_once("connectDB.php");
+        $rname = $_POST['rname'];
+        $sql2 = "INSERT INTO `regions` VALUES (NULL, '{$_POST['rname']}')";
+        mysqli_query($conn,$sql2) or die ("insert ไม่ได้");
+    }
 
 ?>
-
-<table border = "1">
-  <tr>
-     <th>รหัสภาค</th>
-     <th>ชื่อภาค</th>
-   </tr>
-   
+<table border= "1">
+    <tr>
+        <th>รหัสภาค</th>
+        <th>ชื่อภาค</th> 
+        <th>ลบ</th>
+    </tr>
 <?php
-    include_once("connectdb.php");
-	$sql="SELECT*FROM `regions` ORDER BY 'r_name' ASC";
-	$rs = mysqli_query($conn , $sql);
-	
-	while($data = mysqli_fetch_array($rs)){
-	
-?> 
-   <tr>
-     <td><?php echo $data['r_id'];?></td>
-     <td><?php echo $data['r_name'];?></td>
-   </tr>
-<?php } ?>
+    include_once("connectDB.php");
+    $sql = "SELECT * FROM `regions` ORDER BY `r_id` ASC";
+    $rs = mysqli_query($conn , $sql); 
+
+    while($data = mysqli_fetch_array($rs)){
+?>
+    <tr>
+        <td><?php echo $data['r_id']; ?></td>
+        <td><?php echo $data['r_name']; ?></td>
+        <td width="50" align="center">
+            <a href="delete_regions.php?id=<?php echo $data['r_id']; ?>" 
+               onclick="return confirm('ยืนยันการลบ');">
+                <img src="img/images.png" width="20">
+            </a>
+        </td>
+    </tr>
+
+<?php } ?> 
+
+
 </table>
 
 </body>
--- j/b.php
+++ j/b.php
@@ -6,65 +6,77 @@
 </head>
 
 <body>
+    <h1>งาน i</h1>
+    <h1>สุชาวดี สุระสิงห์(ใบเตย)</h1>
 
-<h1>ข้อมูลจังหวัด -- สุชาวดี สุระสิงห์(ใบเตย)</h1>
+    <form method="post" action="" enctype="multipart/form-data">
+        ชื่อจังหวัด: <input type="text" name="pname" required autofocus><br><br>
+    
+        เลือกภาค: 
+        <select name="rid">
+            <?php
+                include_once("connectdb.php");
+                $sql3 = "SELECT * FROM regions ORDER BY r_name ASC";
+                $rs3 = mysqli_query($conn, $sql3);
+                while($data3 = mysqli_fetch_array($rs3)) {
+            ?> 
+                <option value="<?php echo $data3['r_id']; ?>"><?php echo $data3['r_name']; ?></option>
+            <?php } ?> 
+        </select><br><br>
 
-<form method="post" action="" enctype="multipart/form-data">
-    ชื่อจังหวัด
-    <input type="text" name="rname" autofocus required><br>
-
-    รูปภาพ
-    <input type="file" name="pimage"><br>
-
-    ชื่อภาค
-    <select name="rid">
-<?php
-include_once("connectdb.php");
-$sql3 = "SELECT * FROM regions ORDER BY r_name ASC";
-$rs3  = mysqli_query($conn, $sql3);
-
-while($data3 = mysqli_fetch_array($rs3)) {
-?>
-        <option value="<?php echo $data3['r_id']; ?>">
-            <?php echo $data3['r_name']; ?>
-        </option>
-<?php } ?>
-    </select><br><br>
-
-    <button type="submit" name="Submit">บันทึก</button>
-</form>
-
-<br><br>
-
-<table border="1">
-<tr>
-  <th>รหัส</th>
-  <th>ชื่อจังหวัด</th>
-  <th>รูปภาพ</th>
-  <th>ภาค</th>
-</tr>
+        เลือกรูปภาพ: <input type="file" name="pimage" accept="image/*" required><br><br>
+    
+        <button type="submit" name="Submit"> บันทึก </button>
+    </form>
 
 <?php
-include_once("connectdb.php");
-
-$sql = "SELECT * FROM provinces AS p
-INNER JOIN regions AS r
-ON p.r_id = r.r_id
-ORDER BY p.p_name ASC";
-
-$rs = mysqli_query($conn, $sql);
-
-while ($data = mysqli_fetch_array($rs)) {
+    if(isset($_POST['Submit'])){
+        include_once("connectdb.php");
+        
+        $pname = $_POST['pname'];
+        $rid = $_POST['rid'];
+        $ext = pathinfo($_FILES['pimage']['name'], PATHINFO_EXTENSION);
+        $sql_insert = "INSERT INTO provinces (p_id, p_name, r_id, p_ext) VALUES (NULL, '$pname', '$rid', '$ext')";
+       
+        if(mysqli_query($conn, $sql_insert)){
+            
+            $last_id = mysqli_insert_id($conn);
+            $file_name = $last_id . "." . $ext;
+            copy($_FILES['pimage']['tmp_name'], "img/" . $file_name);
+            
+            echo "<script>alert('เพิ่มข้อมูลสำเร็จ'); window.location.href=window.location.href;</script>";
+        } else {
+            echo "Insert ไม่ได้: " . mysqli_error($conn);
+        }
+    }
 ?>
-<tr>
-  <td><?php echo $data['p_id']; ?></td>
-  <td><?php echo $data['p_name']; ?></td>
-  <td>
-    <img src="img/<?php echo $data['p_id']; ?>.<?php echo $data['p_ext']; ?>" width="120">
-  </td>
-  <td><?php echo $data['r_name']; ?></td>
-</tr>
-<?php } ?>
+
+<br><hr><br>
+
+<table border="1" cellpadding="5">
+    <tr>
+        <th>รหัสจังหวัด</th> 
+        <th>ชื่อจังหวัด</th> 
+        <th>รูปภาพ</th> 
+        <th>ภาค</th>
+    </tr>
+<?php
+    $sql = "SELECT * FROM provinces AS p 
+            INNER JOIN regions AS r ON p.r_id = r.r_id
+            ORDER BY p.p_id ASC";
+    $rs = mysqli_query($conn , $sql); 
+
+    while($data = mysqli_fetch_array($rs)){
+?>
+    <tr>
+        <td><?php echo $data['p_id'];?></td>
+        <td><?php echo $data['p_name'];?></td>
+        <td>
+            <img src="img/<?php echo $data['p_id']; ?>.<?php echo $data['p_ext']; ?>" width="100">
+        </td>
+        <td><?php echo $data['r_name'];?></td>
+    </tr>
+<?php } ?> 
 
 </table>
 
